962e5f1d4d0e62495ae38d65e3f589a0e57c519c
Mark <mark@remarkablemark.org>
2022-09-17 22:11:28 -0400

    feat: add Button component
    https://parceljs.org/languages/vue/#vue-3-features

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div>Hello {{ name }}!</div>
+  <Button />
 </template>
 
 <script>
+import Button from './Button.vue';
+
 export default {
+  components: {
+    Button,
+  },
+
   data() {
     return {
       name: 'Vue',
